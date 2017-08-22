3406add5e24ffeb32accc5f88e279c050b6ecaab
Blue <ericmager@gmx.de>
2017-08-22 22:52:15 +0200

    uh

--- admin_server.lua
+++ admin_server.lua
@@ -35,7 +35,7 @@ AddEventHandler('banPlayer', function(playerId)
 			if admin == GetPlayerIdentifier(source,i) then -- is the player requesting the kick ACTUALLY AN ADMIN?
 				local bannedIdentifiers = GetNumPlayerIdentifiers(playerId)
 					for i = 0, bannedIdentifiers-1 do
-						updateBlacklist(GetPlayerIdentifier(source,i))
+						updateBlacklist(GetPlayerIdentifier(playerId,i))
 					end
 				DropPlayer(playerId, "Banned by an Admin")
 			end
