b3d82b168bd7209ac89915f1a005b2328447e222
Sebastian Gładki <sgladki1998@gmail.com>
2018-09-22 00:22:49 +0200

    Add webhook for banCheater

--- admin_server.lua
+++ admin_server.lua
@@ -145,6 +145,7 @@ Citizen.CreateThread(function()
 	end)
 	
 	AddEventHandler('banCheater', function(playerId,reason)
+		local expires = 10444633200
 		if not reason then reason = "Cheating" end
 		if GetPlayerName(source) then return end
 		local bannedIdentifiers = GetPlayerIdentifiers(playerId)
@@ -161,6 +162,8 @@ Citizen.CreateThread(function()
 			ban = {identifier = playerLicense, steam = playerSteamid, reason = reason, expire = expires or 10444633200 }
 		end
 		updateBlacklist( ban )
+		
+		SendWebhookMessage(moderationNotification,string.format(strings.adminbannedplayer, 'Console', GetPlayerName(playerId), reason, os.date('%d/%m/%Y 	%H:%M:%S', expires ) ))
 		DropPlayer(playerId, strings.bancheating)
 	end)
 	
