0eeed3a93fb543e2ae8ef5b2f7c975f9ae2eb39a
Eric Mager <iamabluewolf@gmail.com>
2019-01-26 16:24:35 +0100

    Revert "unfuck ver check and update version"
    This reverts commit c30026c15336221589155816bd10bfe48c97d50d.

--- admin_server.lua
+++ admin_server.lua
@@ -569,7 +569,20 @@ Citizen.CreateThread(function()
 	local curVersion = json.decode(verFile).version
 	local updatePath = "/Bluethefurry/EasyAdmin"
 	local resourceName = "EasyAdmin ("..GetCurrentResourceName()..")"
-	function checkVersion()
+	function checkVersion(err,response, headers)
+		local data = json.decode(response)
+		
+		
+		if curVersion ~= data.version and tonumber(curVersion) < tonumber(data.version) then
+			print("\n--------------------------------------------------------------------------")
+			print("\n"..resourceName.." is outdated.\nCurrent Version: "..data.version.."\nYour Version: "..curVersion.."\nPlease update it from https://github.com"..updatePath.."")
+			print("\nUpdate Changelog:\n"..data.changelog)
+			print("\n--------------------------------------------------------------------------")
+		elseif tonumber(curVersion) > tonumber(data.version) then
+			print("Your version of "..resourceName.." seems to be higher than the current version.")
+		else
+			print(resourceName.." is up to date!")
+		end
 		local nativeuitest = LoadResourceFile("NativeUI", "__resource.lua")
 		if not nativeuitest then
 			print("\n--------------------------------------------------------------------------")
@@ -594,7 +607,7 @@ Citizen.CreateThread(function()
 	end
 	
 	function checkVersionHTTPRequest()
-		checkVersion()
+		PerformHttpRequest("https://raw.githubusercontent.com/Bluethefurry/EasyAdmin/master/version.json", checkVersion, "GET")
 	end
 	
 	function loopUpdateBlacklist()
