b5c3c78986ba9f4f611a1d3511cd00a56d88593c
Eric Mager <iamabluewolf@gmail.com>
2019-03-26 13:57:56 +0100

    remove dependency and nativeui reference from resource

--- __resource.lua
+++ __resource.lua
@@ -1,14 +1,12 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
-dependency 'NativeUI'
-
 server_scripts {
 	"admin_server.lua",
 	"version",
 }
 
 client_scripts {
-	"@NativeUI/NativeUI.lua",
+	"dependencies/NativeUI.lua",
 	"admin_client.lua",
 	"gui_c.lua",
 }
