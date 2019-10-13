c3c5622fb88da1e9c034cd7df451f8ce26bca349
Eric Mager <iamabluewolf@gmail.com>
2019-10-13 11:18:52 +0200

    add missing util function

--- util_shared.lua
+++ util_shared.lua
@@ -19,3 +19,11 @@ function PrintDebugMessage(msg)
 		end
 	end
 end
+
+function math.round(num, numDecimalPlaces)
+	if numDecimalPlaces and numDecimalPlaces>0 then
+		local mult = 10^numDecimalPlaces
+		return math.floor(num * mult + 0.5) / mult
+	end
+	return math.floor(num + 0.5)
+end
