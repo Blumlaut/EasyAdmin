6dc4c71ceaead0c1d8f3f710661e39fb2cf46b9e
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-05-01 18:42:28 +0200

    add example usage of shared and permissions

---
+++ plugins/example_shared.lua
@@ -0,0 +1,7 @@
+
+--[[ This code is disabled as it's an example.
+
+permissions["plugin.exmaple"] = false -- adds "easyadmin.plugin.example" Permission which can be used from both the clientside and server side to check for permissions.
+
+
+]]
