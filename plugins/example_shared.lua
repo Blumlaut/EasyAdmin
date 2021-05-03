e830591b47988422e3902f549897c3ef55f3da36
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-05-03 08:22:24 +0200

    Update example_shared.lua

--- plugins/example_shared.lua
+++ plugins/example_shared.lua
@@ -1,7 +1,8 @@
 
 --[[ This code is disabled as it's an example.
 
-permissions["plugin.exmaple"] = false -- adds "easyadmin.plugin.example" Permission which can be used from both the clientside and server side to check for permissions.
-
+Citizen.CreateThread(function() -- needs to be in a thread.
+  permissions["plugin.exmaple"] = false -- adds "easyadmin.plugin.example" Permission which can be used from both the clientside and server side to check for permissions.
+end
 
 ]]
