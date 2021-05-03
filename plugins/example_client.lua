07c5ddfd20fcf3a4d05f98fbaa8630dca735ff4e
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-05-03 08:32:54 +0200

    add EasyAdmin:MenuRemoved event

--- plugins/example_client.lua
+++ plugins/example_client.lua
@@ -1,6 +1,8 @@
 
 -- EasyAdmin Plugin Example, this allows injecting new UI Elements directly into EasyAdmin Menus, see NativeUILua Docs on Guides how to add new Items, below is an example code.
 
+local somevalue = false
+
 AddEventHandler("EasyAdmin:BuildPlayerOptions", function(playerId) -- BuildPlayerOptions is triggered after building options like kick, ban.. Passes a Player ServerId
 	
 	-- comment this out if you want to see it in action
@@ -11,7 +13,9 @@ AddEventHandler("EasyAdmin:BuildPlayerOptions", function(playerId) -- BuildPlaye
 	local thisItem = NativeUI.CreateItem("Example Item","Player ID is "..playerId) -- create our new item
 	thisPlayer:AddItem(thisItem) -- thisPlayer is global.
 	thisItem.Activated = function(ParentMenu,SelectedItem)
-		-- do stuff
+		-- enter your clientside code here, this will be run once the button has been activated.
+		somevalue = true -- set some value we want to undo once the menu closes.
+
 	end
 
 	if permissions["kick"] then -- you can also check if a user has a specific Permission.
@@ -32,4 +36,9 @@ AddEventHandler("EasyAdmin:BuildServerManagementOptions", function() -- Options 
 end)
 
 AddEventHandler("EasyAdmin:BuildSettingsOptions", function() -- Options for the Settings Page, once again, passes nothing
+end)
+
+AddEventHandler("EasyAdmin:MenuRemoved", function() -- this triggers if a player closes the menu or the menupool gets removed, this CAN trigger multiple times in a row.
+	somevalue = false -- reset our value :)
+
 end)
