50f8d23c6c7c20a420548728d962b741b2abc653
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-05-05 20:50:54 +0200

    be less mean, less mean is always good, right?!

--- admin_server.lua
+++ admin_server.lua
@@ -174,16 +174,12 @@ end
 function getAllPlayerIdentifiers(playerId) --Gets all info that could identify a player
 	local identifiers = GetPlayerIdentifiers(playerId)
 	local tokens = {}
-	if not GetNumPlayerTokens or not GetPlayerToken then
-		Citizen.Trace("^1EasyAdmin: WARNING!!!!^7\n")
-		Citizen.Trace("^1EasyAdmin: WARNING!!!!^7\n")
-		print("^1EasyAdmin^7: You didn't follow the Updating Instructions! Please make sure you Update your FXServer to ^3*at least*^7 Build 3335.")
-		print("^1EasyAdmin^7: For the next time, please read the update instructions and ^1keep your Server updated!!")
-		Citizen.Trace("^1EasyAdmin: WARNING!!!!^7\n")
-		Citizen.Trace("^1EasyAdmin: WARNING!!!!^7\n")
-		return identifiers
-	end
 	if GetConvar("ea_useTokenIdentifiers", "true") == "true" then
+		if not GetNumPlayerTokens or not GetPlayerToken then
+			print("^1EasyAdmin^7: Server Version is below artifact 3335, disabling Token identifiers, please consider updating your FXServer!")
+			SetConvar("ea_useTokenIdentifiers", "false")
+			return identifiers
+		end
 		for i=0,GetNumPlayerTokens(playerId) do
 			table.insert(tokens, GetPlayerToken(playerId, i))
 		end
