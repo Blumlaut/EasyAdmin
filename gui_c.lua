d2686ce7778cf71277862c6329d3e9bbcb61fa78
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-07-24 19:59:08 +0200

    add identifier as rightlabel AND description

--- gui_c.lua
+++ gui_c.lua
@@ -1030,6 +1030,7 @@ function GenerateMenu() -- this is a big ass function
 						for _, identifier in pairs(banlist[i].identifiers) do
 							if not (GetConvar("ea_IpPrivacy", "false") == "true" and string.split(identifier, ":")[1] == "ip") then
 								local thisItem = NativeUI.CreateItem(string.format(GetLocalisedText("identifier"), string.split(identifier, ":")[1]),identifier)
+								thisItem:RightLabel(identifier)
 								mainMenu:AddItem(thisItem)
 								thisItem.Activated = function(ParentMenu,SelectedItem)
 									--nothing
