2ba9da95e21f68f22f3be2b397836b8c190b02e1
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-09-25 23:45:06 +0200

    tweak: add version dependency, some metadata to manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,13 @@ fx_version "cerulean"
 
 games {"rdr3","gta5"}
 
+author 'Blumlaut <blue@furfag.de>'
+description 'EasyAdmin - Admin Menu for FiveM & RedM'
+
+dependencies {
+    '/server:3335', 
+}
+
 lua54 'yes'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
