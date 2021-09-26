8910adf310702dbd005c6252ad3dd9c0234dcc2b
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-09-26 20:49:39 +0200

    feet: example plugin for pNotify notifications

---
+++ plugins/pNotify/pNotify_client.lua
@@ -0,0 +1,22 @@
+--
+-- This is an example Plugin to pass EasyAdmin's Internal events to an External Resource as a Notification, in our example pNotify, the reader is expected to form their own functions from this, editing this code is *NOT* recommended,
+-- as updates will overwrite it, instead, make your own plugin from this example (copying the file and renaming it is enough.)
+-- the end result will look like this: https://blumlaut.me/s/H9pHGsXgjAFeHDP/preview
+--
+
+
+local PnOptions = {layout = "topRight", type = "alert"}
+function pushNotification(options)
+	TriggerEvent("pNotify:SendNotification", options)
+end
+
+
+AddEventHandler("EasyAdmin:NewReport", function(reportData)
+	if reportData.type == 0 then -- call admin
+		local str = string.format("%s calls for an admin!<br>Reason: %s<br>ID: %s", reportData.reporterName, reportData.reason, reportData.id)
+		pushNotification({layout = "topRight", type = "alert", text = str})
+	else
+		local str = string.format("%s reported <br>%s, Reason: %s<br>ID: %s", reportData.reporterName, reportData.reportedName, reportData.reason, reportData.id)
+		pushNotification({layout = "topRight", type = "alert", text = str})
+	end
+end)
