884f6531546370217279ce35ebafc748f9adef2c
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-10-18 19:38:10 +0200

    tweak:  use json encode instead of random table_to_string function

--- util_shared.lua
+++ util_shared.lua
@@ -127,30 +127,7 @@ end
 
 -- Convert a lua table into a lua syntactically correct string
 function table_to_string(tbl)
-	local result = "{"
-	for k, v in pairs(tbl) do
-		-- Check the key type (ignore any numerical keys - assume its an array)
-		if type(k) == "string" then
-			result = result.."[\""..k.."\"]".."="
-		end
-		
-		-- Check the value type
-		if type(v) == "table" then
-			result = result..table_to_string(v)
-		elseif type(v) == "boolean" then
-			result = result..tostring(v)
-		elseif type(v) == "function" then
-			result = result..tostring(v)
-		else
-			result = result.."\""..v.."\""
-		end
-		result = result..","
-	end
-	-- Remove leading commas from the result
-	if result ~= "" then
-		result = result:sub(1, result:len()-1)
-	end
-	return result.."}"
+	return json.encode(tbl)
 end
 
 function mergeTables(t1, t2)
