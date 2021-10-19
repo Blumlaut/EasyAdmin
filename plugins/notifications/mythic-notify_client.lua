3b5c50419642e8963153004c5df28818569c3246
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2021-10-19 20:57:36 +0200

    refactor(plugins): notifications
    - Also removed the top comment from t-notify and mythic-notify

--- plugins/notifications/mythic-notify_client.lua
+++ plugins/notifications/mythic-notify_client.lua
@@ -1,11 +1,3 @@
---
--- This is an example Plugin to pass EasyAdmin's Internal events to an External Resource as a Notification, in our example pNotify, the reader is expected to form their own functions from this, editing this code is *NOT* recommended,
--- as updates will overwrite it, instead, make your own plugin from this example (copying the file and renaming it is enough.)
--- the end result will look like this: https://blumlaut.me/s/H9pHGsXgjAFeHDP/preview
---
--- Feel free to make Pull Requests to add aditional features for this, this is merely an example of whats possible.
---
-
 enableNotificationReplace = false -- change to true to enable replacement of the default V Notification for EasyAdmin
 
 
