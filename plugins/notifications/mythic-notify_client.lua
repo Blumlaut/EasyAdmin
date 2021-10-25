0fa149a5d1c80b833f7782bee68ddf83561a2312
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2021-10-25 20:27:49 +0200

    fix(plugins/notifis): localizing

--- plugins/notifications/mythic-notify_client.lua
+++ plugins/notifications/mythic-notify_client.lua
@@ -1,8 +1,5 @@
 enableNotificationReplace = false -- change to true to enable replacement of the default V Notification for EasyAdmin
 
-
-local PnOptions = {type = "error"} -- define our pNotify options
-
 -- this bit of code tells EasyAdmin to not draw the V Notification.
 AddEventHandler("EasyAdmin:receivedNotification", function()
 	if GetResourceState("mythic-notify") == "started" or enableNotificationReplace then
@@ -12,8 +9,6 @@ end)
 
 RegisterNetEvent("EasyAdmin:showNotification", function(text, important)
 	if GetResourceState("mythic-notify") == "started" or enableNotificationReplace then
-		local options = PnOptions
-		options.message = text
-		exports['mythic_notify']:SendAlert(options)
+		exports['mythic_notify']:SendAlert('inform', text)
 	end
 end)
