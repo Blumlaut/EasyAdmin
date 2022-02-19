4b3c241a85981b03088e992105147e207d87ffd3
Blue <13604413+Blumlaut@users.noreply.github.com>
2022-02-19 14:37:55 +0100

    feat(bot): playerlist has discord name

--- server/bot/commands/playerlist.js
+++ server/bot/commands/playerlist.js
@@ -8,13 +8,22 @@ module.exports = {
 
 		var players = getPlayers()
 		var table = new AsciiTable()
-		table.setHeading('Id', 'Name')
+		table.setHeading('Id', 'Name', 'Discord')
 		 
 
 		if (players.length != 0) {
 			for (let player of players) {
-				var username = await exports[EasyAdmin].getName(player,true,false)
-				table.addRow(player, username)
+				const user = await exports[EasyAdmin].getCachedPlayer(player)
+				var username = user.name
+				var discordAccount = "N/A"
+
+				for (let identifier of user.identifiers) {
+                    if (identifier.search("discord:") != -1) {
+                        discordAccount = (await client.users.fetch(identifier.substring(identifier.indexOf(":") + 1))).tag
+                    }
+                }
+
+				table.addRow(player, username, discordAccount)
 			}
 		} else {
 			table = "There are no players on the server!"
