b92457c13b7bbd8bab5c7db988ccca7f42e61d5e
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2022-03-07 12:49:08 +0100

    Create  example_server.lua

---
+++ plugins/example_server.lua
@@ -0,0 +1,8 @@
+-- EasyAdmin Plugin Example, you can run any server-code here, below is an example that shows this functionality
+
+if false == false then return end
+
+function foo(bar)
+  return bar.." baz"
+end
+AddEventHandler("EasyAdmin:foo", foo)
