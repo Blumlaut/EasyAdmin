4b85ccf8bac56b4642e312897e82b10a27e9a30e
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2022-03-21 13:47:04 +0100

    tweak(bot): promisify util funcs

--- server/bot/functions.js
+++ server/bot/functions.js
@@ -1,54 +1,58 @@
 // this file contains util functions the bot uses
 
-async function prepareGenericEmbed(message,feature,colour,title,image,customAuthor,description,timestamp) {
+function prepareGenericEmbed(message,feature,colour,title,image,customAuthor,description,timestamp) {
+    return new Promise(function(resolve) {
+        if (feature && exports[EasyAdmin].isWebhookFeatureExcluded(feature)) {
+            resolve()
+        }
 
-    if (feature && await exports[EasyAdmin].isWebhookFeatureExcluded(feature)) {
-        return
-    }
+        const embed = new EmbedBuilder()
+        .setColor(colour || 65280)
+        if (timestamp != false) {
+            embed.setTimestamp()
+        }
+        if (message) {
+            embed.addFields({name: `**${(title || "EasyAdmin")}**`, value: message})
+        }
+        if (description) {
+            embed.setDescription(description)
+        }
 
-    const embed = new EmbedBuilder()
-    .setColor(colour || 65280)
-    if (timestamp != false) {
-        embed.setTimestamp()
-    }
-    if (message) {
-        embed.addFields({name: `**${(title || "EasyAdmin")}**`, value: message})
-    }
-    if (description) {
-        embed.setDescription(description)
-    }
+        if (customAuthor) {
+            embed.setAuthor(customAuthor)
+        }
 
-    if (customAuthor) {
-        embed.setAuthor(customAuthor)
-    }
+        if (image) {
+            embed.setImage(image)
+        }
 
-    if (image) {
-        embed.setImage(image)
-    }
-
-    return embed
+        resolve(embed)
+    })
 }
 
-async function findPlayerFromUserInput(input) {
-    var user = undefined
+function findPlayerFromUserInput(input) {
+    return new Promise(function(resolve) {
 
-    var players = await exports[EasyAdmin].getCachedPlayers()
+        var user = undefined
 
-    Object.keys(players).forEach(function(key) {
-        var player = players[key]
-        var name = player.name
-        if(!isNaN(input)) {
-            if (player.id == input) {
-                user = player
+        var players = exports[EasyAdmin].getCachedPlayers()
+
+        Object.keys(players).forEach(function(key) {
+            var player = players[key]
+            var name = player.name
+            if(!isNaN(input)) {
+                if (player.id == input) {
+                    user = player
+                }
+            } else {
+                if (name.search(input) != -1) {
+                    user = player
+                }
             }
-        } else {
-            if (name.search(input) != -1) {
-                user = player
-            }
-        }
+        })
+
+        resolve(user)
     })
-
-    return user
 }
 
 
@@ -65,13 +69,11 @@ function DoesGuildMemberHavePermission(member, object) { // wrapper for Discord 
         }
     
         if (member.guild.ownerId === memberId) { // guild owner always has permissions, to everything.
-            console.log("guild owner. blergh.")
-            //resolve(true)
+            resolve(true)
         }
     
     
         var allowed=IsPrincipalAceAllowed(`identifier.discord:${memberId}`, object)
-        console.log(allowed)
         resolve(allowed)
 
 
@@ -81,35 +83,38 @@ function DoesGuildMemberHavePermission(member, object) { // wrapper for Discord 
 }
 
 
-async function getDiscordAccountFromPlayer(user) {
-    var discordAccount = false
-    if (!isNaN(user)) {
-        user = await exports[EasyAdmin].getCachedPlayer(user)
-    }
-
-    for (let identifier of user.identifiers) {
-        if (identifier.search("discord:") != -1) {
-            discordAccount = await client.users.fetch(identifier.substring(identifier.indexOf(":") + 1))
+function getDiscordAccountFromPlayer(user) {
+    return new Promise(function(resolve) {
+        var discordAccount = false
+        if (!isNaN(user)) {
+            user = exports[EasyAdmin].getCachedPlayer(user)
         }
-    }
 
-    return discordAccount
+        for (let identifier of user.identifiers) {
+            if (identifier.search("discord:") != -1) {
+                discordAccount = client.users.fetch(identifier.substring(identifier.indexOf(":") + 1))
+            }
+        }
+
+        resolve(discordAccount)
+    })
 }
 
 
-async function getPlayerFromDiscordAccount(user) {
-    var id = user.id
+function getPlayerFromDiscordAccount(user) {
+    return new Promise(function(resolve) {
+        var id = user.id
 
-    var players = await exports[EasyAdmin].getCachedPlayers()
+        var players = exports[EasyAdmin].getCachedPlayers()
 
-    for (let [index, player] of Object.values(players).entries()) {
-        for (let identifier of player.identifiers) {
-            if (identifier == `discord:${id}`) {
-                return player
+        for (let [index, player] of Object.values(players).entries()) {
+            for (let identifier of player.identifiers) {
+                if (identifier == `discord:${id}`) {
+                    resolve(player)
+                }
             }
-		}
-    }
+        }
 
-    return false
-
+        resolve(false)
+    })
 }
