b3b146038a766e14713c4b9c1193b246e96f5f3f
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2022-03-29 13:18:26 +0200

    fix(bot): dont attempt to log report if bot isnt configed

--- server/bot/reports.js
+++ server/bot/reports.js
@@ -23,59 +23,21 @@ function generateReportEmbed(report, disabled, closed) {
         {name:"Reason", value: `\`\`\`\n${report.reason}\`\`\``},
         {name:"Report ID", value: `#${report.id}`, inline: true},
         {name:"Claimed by", value:`${(report.claimedName || "Noone")}`, inline: true})
-        
-        
-        
-        /* 
-        const row = new ActionRow()
-        
-        var button = new ButtonComponent()
-        
-        button.setCustomId(`report_close${reportId}`)
-        button.setLabel('Close Report')
-        
-        if (disabled) {
-            button.setDisabled(true)
-        }
-        
-        row.addComponents(button)
-        */
-        
-    return {embeds: [embed], /* components: [row] */}
+
+    return {embeds: [embed]}
     
 }
     
 async function logNewReport(report) {
-    var reportId = report.id
-    reports[reportId] = report
-    
-    
-    
-    var reportMessage = generateReportEmbed(report)
-    
-    
-    var msg = await client.channels.cache.get(GetConvar("ea_botLogChannel", "")).send(reportMessage)
-    reports[reportId].msg = msg
-    
-    
-    /*
-    const filter = i => (i.customId === `report_close${reportId}`);
-    
-    const collector = interaction.channel.createMessageComponentCollector({
-        filter,
-        time: 240000
-    });
-    
-    collector.on('collect', async i => {
-        if (i.customId === `report_close${reportId}` && await DoesGuildMemberHavePermission(i.member, "player.reports.process")) {
-            var reportMessage = generateReportEmbed(report, true, true)
-            
-            msg.edit(reportMessage)
-        }
-        await i.deferUpdate()
-    });
-    */
-    
+    if (GetConvar("ea_botToken", "") != "") {
+        var reportId = report.id
+        reports[reportId] = report
+        var reportMessage = generateReportEmbed(report)
+        var msg = await client.channels.cache.get(GetConvar("ea_botLogChannel", "")).send(reportMessage)
+        reports[reportId].msg = msg
+    } else {
+        return false
+    }
 }
 
 
