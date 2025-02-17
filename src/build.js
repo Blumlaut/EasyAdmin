9dc9e07901bcd8cfcd3a2991b52cdbaa4ec1f769
blumlaut <13604413+Blumlaut@users.noreply.github.com>
2025-02-18 00:56:46 +0100

    tweak(bot): remove comments
    not needed here

--- src/build.js
+++ src/build.js
@@ -1,14 +1,14 @@
 const esbuild = require("esbuild");
 
 esbuild.build({
-  entryPoints: ["bot/**/*.js"], // Change this to your main script
+  entryPoints: ["bot/**/*.js"],
   bundle: true,
   minify: false,
   platform: "node",
-  target: "node22", // Match your node_version
+  target: "node22", 
   outdir: "../dist",
   treeShaking: true,
   ignoreAnnotations: true,
   define: { "process.env.NODE_ENV": '"production"' },
-  external: ["zlib-sync"], // These modules may need native bindings and should be installed on the server
+  external: ["zlib-sync"], 
 }).catch(() => process.exit(1));
