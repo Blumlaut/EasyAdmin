4a3aebe6bf2972df67e10bb80ec706f34b25d691
blumlaut <13604413+Blumlaut@users.noreply.github.com>
2025-02-18 21:12:00 +0100

    refactor(eslint): change config format to mjs

---
+++ src/eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "script",
+    },
+
+    rules: {
+        indent: ["error", "tab"],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "never"],
+        "no-undef": "off",
+        "no-else-return": ["warn"],
+
+        "no-empty-function": ["error", {
+            allow: ["arrowFunctions"],
+        }],
+
+        "no-implicit-coercion": ["warn"],
+        "no-useless-return": ["warn"],
+        "no-undef-init": ["warn"],
+    },
+}];
