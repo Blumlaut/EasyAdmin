be9ac0192154302fe49277beb527bc59d6e053ed
blumlaut <13604413+Blumlaut@users.noreply.github.com>
2025-02-18 21:25:22 +0100

    refactor(eslint): final fix commit

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,42 +1,43 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+})
 
-export default [...compat.extends("eslint:recommended"), {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-        },
+export default [...compat.extends('eslint:recommended'), {
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.commonjs,
+		},
 
-        ecmaVersion: "latest",
-        sourceType: "script",
-    },
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 
-    rules: {
-        indent: ["error", "tab"],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single"],
-        semi: ["error", "never"],
-        "no-undef": "off",
-        "no-else-return": ["warn"],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'no-undef': 'off',
+		'no-unused-vars': 'warn',
+		'no-else-return': ['warn'],
 
-        "no-empty-function": ["error", {
-            allow: ["arrowFunctions"],
-        }],
+		'no-empty-function': ['error', {
+			allow: ['arrowFunctions'],
+		}],
 
-        "no-implicit-coercion": ["warn"],
-        "no-useless-return": ["warn"],
-        "no-undef-init": ["warn"],
-    },
-}];
+		'no-implicit-coercion': ['warn'],
+		'no-useless-return': ['warn'],
+		'no-undef-init': ['warn'],
+	},
+}]
